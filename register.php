685dc27e9e8ae95bf89f84c4fe6977d832af4364
rediatbrook <rediatbrook@gmail.com>
2021-07-26 16:52:40 -0500

    Login with FB and Google removed for now

--- register.php
+++ register.php
@@ -42,15 +42,7 @@ include("./html/maintheme.html");
                         <a href="login.html" class="btn btn-primary btn-user btn-block">
                             Register Account
                         </a>
-                        <hr>
-                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                            <i class="fab fa-google fa-fw"></i> Register with Google
-                        </a>
-                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                            <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                        </a>
                     </form>
-                    <hr>
                     <div class="text-center">
                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                     </div>
