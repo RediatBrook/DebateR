ecc8ec4a5b018b0c82574c2b42af5ca512c3a4b8
rediatbrook <rediatbrook@gmail.com>
2021-07-30 15:13:46 -0500

    Added basic form validation

--- js/register.js
+++ js/register.js
@@ -1,3 +1,17 @@
-function submitter(){
-    document.getElementById("rgform").submit();
+function validate(){
+    let p1 = document.forms["rgform"]["password"].value;
+    let p2 = document.forms["rgform"]["passwordr"].value;
+    if(p1===p2){
+        if(p1.length<8){
+            alert("Password should contain atleast 8 characters");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        alert("Password entered in the first field and second field are different.");
+        return false;
+    }
 }
--- register.php
+++ register.php
@@ -15,29 +15,29 @@ include("./html/prelogin.html");
                     <div class="text-center">
                         <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                     </div>
-                    <form id="rgform" class="user" action="createaccount.php" method="post">
+                    <form id="rgform" class="user" onsubmit = "return validate()" action="createaccount.php" method="post">
                         <div class="form-group row">
                             <div class="col-sm-6 mb-3 mb-sm-0">
                                 <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                    placeholder="First Name" name="firstname">
+                                    placeholder="First Name" name="firstname" required>
                             </div>
                             <div class="col-sm-6">
                                 <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                    placeholder="Last Name" name="lastname">
+                                    placeholder="Last Name" name="lastname" required>
                             </div>
                         </div>
                         <div class="form-group">
                             <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                placeholder="Email Address" name="email">
+                                placeholder="Email Address" name="email" required>
                         </div>
                         <div class="form-group row">
                             <div class="col-sm-6 mb-3 mb-sm-0">
                                 <input type="password" class="form-control form-control-user"
-                                    id="exampleInputPassword" placeholder="Password" name="password">
+                                    id="exampleInputPassword" placeholder="Password" name="password" required>
                             </div>
                             <div class="col-sm-6">
-                                <input type="password" class="form-control form-control-user"
-                                    id="exampleRepeatPassword" placeholder="Repeat Password">
+                                <input type="password" name="passwordr" class="form-control form-control-user"
+                                    id="exampleRepeatPassword" placeholder="Repeat Password" required>
                             </div>
                         </div>
                         <input type="submit" value="Register Account"href="#" class="btn btn-primary btn-user btn-block">
@@ -57,4 +57,4 @@ include("./html/prelogin.html");
     </div>
 </div>
 </div>
-<script src="register.js"></script>
+<script src="./js/register.js"></script>
